3c270a9b7edffa5f7a9883472f19c76a9143bbf9
kicks <kicks@KICKSCAR-LAPTOP>
2018-03-03 13:12:56 +0900

    init commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int i = 10;
+		String s = "hello";
+		//String s = new String( "hello" );
+		
+		Goods g = new Goods();
+		g.setName( "콜라" );
+		g.setPrice( 2000 );
+		
+		int a = -1;
+		g.setPrice( a );
+		
+		System.out.println( g.getName() + ":" + g.getPrice() );
+		
+		Goods g2 = new Goods("사이다", 2000);
+		System.out.println( g2.getName() + ":" + g2.getPrice() );
+	}
+}
